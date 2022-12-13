5cc142941e063fc669424cf6aac588676242fd1a
SunTeany <120448045+SunTeany@users.noreply.github.com>
2022-12-13 14:22:41 +0700

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,46 @@
+// function welcome(){
+//     let name = "jonh";
+//     console.log(`Welcome ${name}`);
+// }
+// welcome();
+
+// let welcome = function (name,email,age){
+//     document.write(`Welcome ${name} `);
+//     document.write(`My Email = ${email}`);
+//     document.write(`and im ${age} year old`);
+// }
+// welcome("Smite","Smite@Gmail.com",20);
+
+// let welcome = function (name,email,age){
+//     console.log(`Welcome ${name} `);
+//     console.log(`My Email = ${email}`);
+//     console.log(`and im ${age} year old`);
+// }
+// welcome("Smite","Smite@Gmail.com",20);
+
+// function showMessage() {
+//     alert( 'Hello everyone!' );
+//   }
+
+// showMessage();
+
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } 
+//       return confirm('Did parents allow you?');
+//   }
+// checkAge();
+
+// function checkAge(age) {
+//     return (age > 18)  ? true : confirm('Did parents allow you?');
+//   }
+// function checkAge(age) {
+//     return (age > 18)  || confirm('Did parents allow you?');
+//   }
+function min(a="2", b="3") {
+    return a < b ? a : b;
+  }
+  return confirm('Did parents allow you?');
+
+min();
